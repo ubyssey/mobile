3af27a167fb5391d1c67674000feb877ef19fe7b
Ron Y <31330116+i-ron-y@users.noreply.github.com>
2018-03-21 13:25:39 -0700

    Create article content component

---
+++ src/Article.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Article.css';
+
+class Article extends Component {
+  render() {
+    return (
+      <div className="Article">
+      </div>
+    );
+  }
+}
+
+export default Article;
