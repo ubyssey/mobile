b214359f33fda4fabdc29c00f75dd76c18af102b
Ron Y <31330116+i-ron-y@users.noreply.github.com>
2018-03-21 12:46:12 -0700

    Fix Header.css to adhere to style guide

--- src/Header.js
+++ src/Header.js
@@ -5,9 +5,9 @@ import './Header.css';
 class Header extends Component {
   render() {
     return (
-      <div className="Header">
-      	<a class="left" href="#"><i class="fa fa-bars"></i></a>
-      	<a class="logo" href="/"><img src={logo} type="text/css" /></a>
+      <div className="c-mobile-header">
+      	<a class="c-mobile-header-left" href="#"><i class="fa fa-bars"></i></a>
+      	<a class="c-mobile-header-logo" href="/"><img src={logo} type="text/css" /></a>
       </div>
     );
   }
