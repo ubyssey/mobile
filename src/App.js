fa726183a5dd35241a5977e1ccf1c583e4df3fb3
Ron Y <31330116+i-ron-y@users.noreply.github.com>
2018-04-25 01:38:56 -0700

    Display the article selected from the carousel

--- src/App.js
+++ src/App.js
@@ -11,10 +11,21 @@ class App extends Component {
       selected: 0,
       articles: [
         {
+          article_id: 0,
+          headline: 'Article headline here (#0)',
+          author: 'Firstname Lastname',
+          date_published: new Date('04/04/2018'),
+          reading_time: '5',
+          featured_image: './image-3-medium.jpg',
+          category: 'Insert Category Here',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        },
+        {
           article_id: 1,
           headline: 'This is article #1',
-          author: 'Article Author #1',
-          date_published: '2017-10-31',
+          author: 'Author #1',
+          date_published: new Date('10/31/2017'),
+          reading_time: '4',
           featured_image: './Image1.jpg',
           category: 'Category #1',
           content: 'contents of article #1',
@@ -23,7 +34,8 @@ class App extends Component {
           article_id: 2,
           headline: 'This is article #2',
           author: 'Article Author #2',
-          date_published: '2017-11-31',
+          date_published: new Date('11/30/2017'),
+          reading_time: '3',
           featured_image: './Image2.jpg',
           category: 'Category #2',
           content: 'contents of article #2',
@@ -32,7 +44,8 @@ class App extends Component {
           article_id: 3,
           headline: 'This is article #3',
           author: 'Article Author #3',
-          date_published: '2017-12-31',
+          date_published: new Date('12/31/2017'),
+          reading_time: '2',
           featured_image: './Image3.jpg',
           category: 'Category #3',
           content: 'contents of article #3',
@@ -41,7 +54,8 @@ class App extends Component {
           article_id: 4,
           headline: 'This is article #4',
           author: 'Article Author #4',
-          date_published: '2018-01-31',
+          date_published: new Date('01/31/2018'),
+          reading_time: '1',
           featured_image: './Image4.jpg',
           category: 'Category #4',
           content: 'contents of article #4',
@@ -50,7 +64,8 @@ class App extends Component {
           article_id: 5,
           headline: 'This is article #5',
           author: 'Article Author #5',
-          date_published: '2018-02-31',
+          date_published: new Date('02/28/2018'),
+          reading_time: '0',
           featured_image: './Image5.jpg',
           category: 'Category #5',
           content: 'contents of article #5',
@@ -70,7 +85,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <Article />
+        <Article article={this.state.articles[this.state.selected]} />
         <Footer selected={this.state.selected} articles={this.state.articles} onClick={(i) => this.handleClick(i)} />
       </div>
     );
--- src/Article.js
+++ src/Article.js
@@ -6,31 +6,8 @@ class Article extends Component {
   constructor(props) {
   	super(props);
   	this.state = {
-  	  content: this.getContent(),
   	  expanded: false,
   	}
-  }
-
-  getContent() {
-  	return (
-  	  <div>
-	      <h1>Article headline here</h1>
-        <div className="c-mobile-article__info">
-          <div className="c-mobile-article__info--left">
-            <div className="c-mobile-article__author"><b>Written by Firstname Lastname</b></div>
-          </div>
-          <div className="c-mobile-article__info--right">
-            <div className="c-mobile-article__date">April 4, 2018</div>
-            <div className="c-mobile-article__readtime">5 min read</div>
-          </div>
-        </div>
-        <img src={featuredimage} />
-	  	  <p>
-	        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-	      	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-	      </p>
-      </div>
-    )
   }
 
   handleButtonClick() {
@@ -40,11 +17,31 @@ class Article extends Component {
 
   render() {
   	const toggledClass = this.state.expanded ? 'expanded' : 'collapsed';
+    var image = require(`${this.props.article.featured_image}`);
     return (
       <div className="c-mobile-article">
       	<div className={`c-mobile-article__content ${toggledClass}`}>
-      		{this.state.content}
-      	</div>
+          <h1>{this.props.article.headline}</h1>
+          <div className="c-mobile-article__info">
+            <div className="c-mobile-article__info--left">
+              <div className="c-mobile-article__author"><b>Written by {this.props.article.author}</b></div>
+            </div>
+            <div className="c-mobile-article__info--right">
+              <div className="c-mobile-article__date">
+                {new Intl.DateTimeFormat('en-US', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric'
+                }).format(this.props.article.date_published)}
+              </div>
+              <div className="c-mobile-article__readtime">{this.props.article.reading_time} min read</div>
+            </div>
+          </div>
+          <img src={image} />
+          <p>
+            {this.props.article.content}
+          </p>
+        </div>
       	{this.state.expanded ? '' : (<div id="gradient"></div>)}
       	{this.state.expanded ? '' : (<button onClick={() => this.handleButtonClick()}>Read full article</button>)}
       </div>
